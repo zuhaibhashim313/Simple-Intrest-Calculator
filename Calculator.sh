d424f4a33bce7cd65d0000128f353358b402cdec
zuhaibhashim313 <123729940+zuhaibhashim313@users.noreply.github.com>
2023-01-27 18:07:20 +0500

    Delete Calculator.sh
    Signed-off-by: zuhaibhashim313 <123729940+zuhaibhashim313@users.noreply.github.com>

--- Calculator.sh
+++
@@ -1,9 +0,0 @@
-echo " Enter the principle value: "
-read p
-echo " Enter the rate of interest:"
-read r
-echo " Enter the time period:"
-read t
-s=`expr $p \* $t \* $r / 100`
-echo " The simple interest is "
-echo $s
